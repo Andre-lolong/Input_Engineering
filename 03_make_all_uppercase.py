719f2ad6a1a83ce6e7920eda335eeb4f8e7d4de1
Andre <andrepetilunalolong@gmail.com>
2025-03-20 13:20:35 +0800

    fixed the spaces

--- 03_make_all_uppercase.py
+++ 03_make_all_uppercase.py
@@ -1,5 +1,5 @@
 # ask the user to input their full name
 fullname = input("Enter your fullname: ")
+FULLNAME = fullname.upper() # make the input all in upper 
 
-FULLNAME = fullname.upper() # make the input all in upper case
 print(f"Your fullname: {FULLNAME}") # print the fullname of the user in upper case
