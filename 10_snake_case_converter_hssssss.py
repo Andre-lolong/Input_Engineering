823d44ce1dba7af6586a897de35445960d80ae94
Andre <andrepetilunalolong@gmail.com>
2025-03-20 14:50:03 +0800

    print the full name of user in snake casing

--- 10_snake_case_converter_hssssss.py
+++ 10_snake_case_converter_hssssss.py
@@ -1,3 +1,10 @@
 # ask the user for their fullname in incorrect casing
+fullname = input("Enter your fullname in incorrect casing: ")
+
 # convert to snake case
+FULLNAME = fullname.title()
+word = FULLNAME.split()
+snake_cased_name = '_'.join(word)
+
 # print the user's fullname in snake casing
+print(f"Your name in snake case format is: {snake_cased_name}")
