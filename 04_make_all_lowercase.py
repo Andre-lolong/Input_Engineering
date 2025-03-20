15c16fb14d2f3262aa2142b076ba5cded549d651
Andre <andrepetilunalolong@gmail.com>
2025-03-20 13:21:31 +0800

    fixed the spaaces

--- 04_make_all_lowercase.py
+++ 04_make_all_lowercase.py
@@ -1,5 +1,5 @@
 # ask the user to input their fullname
 fullname = input("Enter your fullname: ")
-
 FULLNAME = fullname.lower() # make the input all in lower case
+
 print(f"Your fullname: {FULLNAME}") # print the fullname of the user in lower case 
