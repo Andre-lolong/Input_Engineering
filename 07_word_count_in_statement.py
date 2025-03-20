8bb2fbc7f677117da5dfe4ecb62b1e2acf82605f
Andre <andrepetilunalolong@gmail.com>
2025-03-20 13:37:35 +0800

    counts the words used in a statement

--- 07_word_count_in_statement.py
+++ 07_word_count_in_statement.py
@@ -1,3 +1,9 @@
 # ask the user to enter any statement
+sentence = input("Enter any statement you want: ")
+
 # count each word
-# print how many words are used in the statement
+word = sentence.split()
+word_count = len(word)
+
+# print how many words are used in the 
+print(f'You have used {word_count} in your statement "{sentence}"')
