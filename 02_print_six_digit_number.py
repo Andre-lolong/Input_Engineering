e1d8cbe5a1a5df4d51966cf4e81753b8f9662858
Andre <andrepetilunalolong@gmail.com>
2025-03-20 11:51:17 +0800

    prints the six digit number

--- 02_print_six_digit_number.py
+++ 02_print_six_digit_number.py
@@ -4,7 +4,6 @@ while True:
         num = input("Enter a number from 1 to 1000: ")
         if 1 <= int(num) <= 1000:
             num = num
-            print(num)
             break
         else:
             print("The number you entered is not between 1 to 1000. Try again.\n")
@@ -12,5 +11,16 @@ while True:
         print("Enter a valid input.")
 # check the digit
 digit = len(num)
-print(digit)
 # add zeros to the beginning to match the six digit requirement for the input
+if digit == 1:
+    print(f"00000{num}")
+elif digit == 2:
+    print(f"0000{num}")
+elif digit == 3:
+    print (f"000{num}")
+elif digit == 4:
+    print(f"00{num}")
+else:
+    print("")
+
+
