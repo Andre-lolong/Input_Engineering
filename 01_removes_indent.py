abf1430cf74d855035907cd175131d97b17674dd
Andre <andrepetilunalolong@gmail.com>
2025-03-20 10:45:23 +0800

    prints the fullname with noo indent

--- 01_removes_indent.py
+++ 01_removes_indent.py
@@ -1,3 +1,5 @@
 # ask the user for their fullname with several spaces in the beginning
-# remove the indentation
-# print the fullname
+fullname = input("Enter your fullname with several space characters at the beginning: ")
+full_name = fullname.strip() # remove the indentation
+
+print(f"Fullname: {full_name}") # print the fullname
