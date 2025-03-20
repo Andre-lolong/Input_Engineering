8564d2e0f258e1b48102d5be97016a515e4080bb
Andre <andrepetilunalolong@gmail.com>
2025-03-20 14:45:04 +0800

    pseudocode

--- 09_CamelCase_converter.py
+++
@@ -1,3 +0,0 @@
-# Ask the user for their fullname in incorrect casing
-# Convert to pascal case
-# print the pascal case format of the user name
---
+++ 10_snake_case_converter_hssssss.py
@@ -0,0 +1,3 @@
+# ask the user for their fullname in incorrect casing
+# convert to snake case
+# print the user's fullname in snake casing
